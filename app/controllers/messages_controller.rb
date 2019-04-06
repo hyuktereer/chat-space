a96a923b19482b9da7720edc386c12ebd4786764
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-04-06 23:59:21 +0900

    Create all-group-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,14 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
   end
+
+  def create
+    Message.create(message_params)
+    move_to_index
+  end
+
+  private
+  def  message_params
+    params.require[:message].permit.params(:body)
+  end
 end
