ca39041cda50364b6ba9fbdb14ef03bf6e0a06f5
hyuktereer <hyuktereer2016@yahoo.co.jp>
2019-04-25 18:37:00 +0900

    Revise reloadMessage function

--- app/assets/javascripts/reloadMessage.js
+++ app/assets/javascripts/reloadMessage.js
@@ -35,7 +35,6 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(data){
-      console.log(data)
       //追加するHTMLの入れ物を作る
       var html = [];
       //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
@@ -53,7 +52,7 @@ $(function(){
       scrollTop: height}, 200);      
     })
     .fail(function(){
-      console.log('errorだよ')
+      alert('errorだよ')
     });
   };  
     //
